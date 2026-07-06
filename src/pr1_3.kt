6940abaaa85c284491df542f444a4fb005899c87
vaiditpatel <vaiditp!8@gmail.com>
2026-07-06 12:22:15 +0530

    commit

---
+++ src/pr1_3.kt
@@ -0,0 +1,25 @@
+fun main () {
+    val i = 10000
+    val j: Double =i.toDouble()
+    println("i= $i")
+    println("j= $j")
+    println("Double value (from Integer:$j")
+
+
+
+    var a : Int = 10
+    var b : Double = a.toDouble()
+    var s : String = "10"
+    var s2 : String = "11.12"
+    var c : Int = s.toInt()
+    var d : Int = s.toInt()
+    var e : Double = s2.toDouble()
+
+    println("Integer value : $a")
+    println("Double value (from Integer): $b")
+    println("String value : $s")
+    println("Integer value1 (from String) : $c")
+    println("Integer value 2 (from String) : $d")
+    println("Double Value (from String): $e")
+
+}
