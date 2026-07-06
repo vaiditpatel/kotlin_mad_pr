4410ef00f8217412ed801411730bb4e8d385b8ae
vaiditpatel <vaiditp!8@gmail.com>
2026-07-06 12:20:51 +0530

    Initial commit

---
+++ src/pr1_1.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var i = 1
+    var j : Int = 2
+    var a : Double  = 14.52
+    var b : String = "R"
+    var c : Char = 't'
+    var d : Boolean = false
+    var e : Float = 3.5f
+    var f : Long  = 346153
+    var g : Short = -4
+    var h : Byte = 127
+
+
+
+    println("a = "+a)
+    println("j = "+j)
+    println("i = "+i)
+    println("b = "+b)
+    println("c = "+c)
+    println("d = "+d)
+    println("e = "+e)
+    println("f = "+f)
+    println("g = "+g)
+    println("h = "+h)
+
+
+
+
+
+}
